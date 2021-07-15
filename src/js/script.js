5b907ea11dbf0b2827194fda7561a1cbf32f0c67
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-07-15 19:13:11 +0200

    add initAccordion method

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
-  'use strict';
+  ('use strict');
 
   const select = {
     templateOf: {
@@ -45,21 +45,74 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+    },
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    menuProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.menuProduct).innerHTML
+    ),
   };
 
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      const generateHTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(generateHTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+      const click = thisProduct.element.querySelector(
+        select.menuProduct.clickable
+      );
+      click.addEventListener('click', function() {
+        event.preventDefault();
+        thisProduct.element.classList.add(classNames.menuProduct.wrapperActive);
+
+        const activeProducts = document.querySelectorAll('.product.active');
+
+        for (let activeProduct of activeProducts) {
+          if (activeProduct != thisProduct.element) {
+            activeProduct.classList.remove(
+              classNames.menuProduct.wrapperActive
+            );
+          }
+        }
+      });
+    }
+  }
   const app = {
-    init: function(){
+    initData: function() {
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+    initMenu: function() {
+      const thisApp = this;
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+    init: function() {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
