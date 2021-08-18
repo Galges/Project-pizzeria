03eefed2c3e84ed0288b8720a6f669f38a8cf493
Mateusz Gałuszka <galuszka1996@gmail.com>
2021-08-18 22:35:39 +0200

    homepage

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
-
+import Home from './components/Home.js';
 
 const app = {
 
@@ -97,13 +97,20 @@ const app = {
     const bookingElem = document.querySelector(select.containerOf.booking);
 
     thisApp.booking = new Booking(bookingElem);
+  },
 
+  initHome: function(){
+    const thisApp = this;
+    const homeSite = document.querySelector(select.containerOf.home);
+
+    thisApp.home = new Home(homeSite);
   },
 
   init: function(){
     const thisApp = this;
     thisApp.initPages();
     thisApp.initData();
+    thisApp.initHome();
     thisApp.initCart();
     thisApp.initBooking();
       
--- src/js/settings.js
+++ src/js/settings.js
@@ -3,12 +3,14 @@ export const select = {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product',
     bookingWidget: '#template-booking-widget',
+    homePage: '#template-home-page',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
     pages: '#pages',
     booking: '.booking-wrapper',
+    home: '.home-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -28,6 +30,9 @@ export const select = {
       input: 'input.amount', 
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
+    },
+    carousel: {
+      wrapper: '.main-carousel',
     },
     datePicker: {
       wrapper: '.date-picker',
@@ -131,4 +136,5 @@ export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
   bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
+  homePage: Handlebars.compile(document.querySelector(select.templateOf.homePage).innerHTML),
 };
